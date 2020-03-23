57a301803fee44ca2ddeb9af6df7b24c41b3fb2f
viren <vincent.renard@gmx.com>
2020-03-23 16:42:17 +0100

    stream subscribe

--- src/main/java/fr/univ/orleans/ws/wsalakon/contoler/MessageController.java
+++ src/main/java/fr/univ/orleans/ws/wsalakon/contoler/MessageController.java
@@ -3,9 +3,12 @@ package fr.univ.orleans.ws.wsalakon.contoler;
 import fr.univ.orleans.ws.wsalakon.model.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.ReplayProcessor;
 
 import java.net.URI;
 import java.util.ArrayList;
@@ -25,29 +28,35 @@ public class MessageController {
 
     private static final Logger log = LoggerFactory.getLogger(MessageController.class);
     private List<Message> messages = new ArrayList<>();
-    private final AtomicLong counter =new AtomicLong(1L);
+    private final AtomicLong counter = new AtomicLong(1L);
+    private ReplayProcessor<Message> notifications = ReplayProcessor.create(0, false);
 
     @PostMapping("/messages")
-    ResponseEntity<Message> create(@RequestBody Message message){
+    ResponseEntity<Message> create(@RequestBody Message message, UriComponentsBuilder baseUrl) {
         log.debug(message.toString());
-        Message m = new Message(counter.getAndIncrement(),message.getTexte());
+        Message m = new Message(counter.getAndIncrement(), message.getTexte());
         messages.add(m);
-        URI loc = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/{id}")
+        notifications.onNext(m);
+        URI loc = baseUrl
+                .path("api/messages/{id}")
                 .buildAndExpand(m.getId()).toUri();
 
-            return ResponseEntity.created(loc).body(m);
+        return ResponseEntity.created(loc).body(m);
     }
 
     @GetMapping("/messages")
-    ResponseEntity<Collection<Message>> getAll(){
+    ResponseEntity<Collection<Message>> getAll() {
         return ResponseEntity.ok(messages);
     }
 
+    @GetMapping(value = "/messages/subscribe", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    Flux<Message> subscribe() {
+        return Flux.from(notifications);
+    }
 
     @GetMapping("/messages/{id}")
-    ResponseEntity<Message> findById(@PathVariable long id){
-        Optional<Message> m = messages.stream().filter(message ->message.getId()==id).findAny();
+    ResponseEntity<Message> findById(@PathVariable long id) {
+        Optional<Message> m = messages.stream().filter(message -> message.getId() == id).findAny();
         if (m.isPresent())
             return ResponseEntity.ok(m.get());
         else
